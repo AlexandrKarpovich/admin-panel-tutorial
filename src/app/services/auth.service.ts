4046d36ff223fe6e34feb8033359d46bc995a885
Alex Karpovich <akarpovich@b2broker.net>
2022-03-23 23:39:01 +0300

    add servise auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token)
+  }
+
+  getToken () {
+    return localStorage.getItem('token')
+  }
+
+  isLoggenIn() {
+    return this.getToken() != null;
+  }
+
+  login(userInfo: {email: string, password: string}): Observable<string | boolean> {
+    if(userInfo.email === 'admin@gmail.com' &&  userInfo.password === 'admin123') {
+      this.setToken('sfsfasfsageewdsadsffsfs')
+      return of(true)
+    }
+    return throwError( () => new Error('Failed Login'))
+  }
 }
