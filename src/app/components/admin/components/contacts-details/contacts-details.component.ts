6ca111cb376ec584a1dd832e9fd89222288496ca
Alex Karpovich <akarpovich@b2broker.net>
2022-03-25 15:58:30 +0300

    contact detail card date

--- src/app/components/admin/components/contacts-details/contacts-details.component.ts
+++ src/app/components/admin/components/contacts-details/contacts-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../user';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-contacts-details',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts-details.component.scss']
 })
 export class ContactsDetailsComponent implements OnInit {
+  
+  id!: number;
+  user!: Observable<User>; 
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => this.id = params?.['id']);
+    this.user = this.adminService.getPerson(this.id);
+    console.log(this.id); 
+    
   }
 
 }
